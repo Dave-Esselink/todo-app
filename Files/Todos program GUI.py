5175a9af795ce4211efa086686d626ce29f480ba
Dave E <dave.esselink@gmail.com>
2024-03-23 15:13:20 -0700

    Implemented the Edit button, live window updates when add and edit is used. Item now displays in the input box when clicked on.

--- Files/Todos program GUI.py
+++ Files/Todos program GUI.py
@@ -3,27 +3,54 @@ import PySimpleGUI as sg                  #imports custom GUI modual. 3rd party 
 
 label = sg.Text("Type in a to-do")        #App user prompt
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-#Input text box. key=The dictonary key in window.read()
+#Input text box. key=The dictonary key in window.read(). Key= is used to access the input box's contents in other code
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=cfun.get_todos(), key='todo_items', enable_events=True, size=[45, 10])
+#Creates a list box and populates it with todos.txt. key=Used to access the contents of the list
+edit_button = sg.Button("Edit")
 
 window = sg.Window('To-Do App',
                    layout=[[label],
-                           [input_box, add_button]],
+                           [input_box, add_button],
+                           [list_box, edit_button]],
                    font=('Helvetica', 10))
                    #Each set of brackets=Another horizontal row
 while True:
 #Keeps the window open
     event, values = window.read()
-    #Multiple variables can be used with tuples. window.read outputs a dictonary. Event=user input, values=dictonary
+    #Multiple variables can be used with tuples. window.read outputs a dictonary. Event=Interface button, values=dictonary
     print(event)
     print(values)
     match event:
+    #Maps the buttons to a function
         case "Add":
+        #Add button functionality
             todos = cfun.get_todos()
             new_todo = values['todo'] + "\n"
+            #selects the dictonary key 'todo' from the input_box.
             todos.append(new_todo)
+            #appends the user input to todos as a dictonary from new_todo
             cfun.write_todos(todos)
+            #writes the data to a file
+            window["todo_items"].update(values=todos)
+            #Updates the window after the item is added to the list
+        case "Edit":
+            todo_edit = values['todo_items'][0]
+            new_todo = values['todo'] + "\n"
+            #todo=input box, todo_items=List window
+            todos = cfun.get_todos()
+            index = todos.index(todo_edit)
+            #Locates the specified item in 'todo_items'
+            todos[index] = new_todo
+            #Updates the item in the todos list
+            cfun.write_todos(todos)
+            window["todo_items"].update(values=todos)
+            #Updates the window after the item is edited
+        case "todo_items":
+            window['todo'].update(value=values['todo_items'][0])
+            #Displays the item in the input box when clicked
         case sg.WIN_CLOSED:
+            #closes the window
             break
 
 window.close()
